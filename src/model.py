5a6186e2f0f60e94298042e5ee3624debd1b64c3
annieyp <annie.yeonwon.park@gmail.com>
2026-07-02 14:50:36 -0400

    changed model to mobilenetv2

--- src/model.py
+++ src/model.py
@@ -2,19 +2,21 @@ import tensorflow as tf
 
 
 def build_model(num_classes):
-    base_model = tf.keras.applications.EfficientNetV2S(
+    inputs = tf.keras.Input(shape=(224, 224, 3))
+    x = tf.keras.applications.mobilenet_v2.preprocess_input(inputs)
+
+    base_model = tf.keras.applications.MobileNetV2(
         weights="imagenet",
         include_top=False,
-        include_preprocessing=True,
         input_shape=(224, 224, 3),
     )
 
     base_model.trainable = False
 
-    x = base_model.output
+    x = base_model(x, training=False)
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dense(256, activation="relu")(x)
     x = tf.keras.layers.Dropout(0.3)(x)
     output = tf.keras.layers.Dense(num_classes, activation="softmax")(x)
 
-    return tf.keras.Model(inputs=base_model.input, outputs=output)
+    return tf.keras.Model(inputs=inputs, outputs=output)
